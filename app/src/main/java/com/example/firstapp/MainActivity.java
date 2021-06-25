e0828e591c8dc995e831e58e7f9c9ce51c0505c4
ShashwatKadariya <kadariyasaswot@gmail.com>
2021-06-25 09:02:50 +0545

    ASSIGNMENT - TableLayout, basic button application on main activity

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_realtive_layout);
+        setContentView(R.layout.activity_main);
     }
 
+    public void handleText(View view)
+    {
+        EditText text = findViewById(R.id.text1);
+        String input = text.getText().toString();
+        TextView texting = findViewById(R.id.text2);
+        texting.setText(input);
+    }
 }
--- app/src/main/java/com/example/firstapp/RealtiveLayout.java
+++ app/src/main/java/com/example/firstapp/RealtiveLayout.java
@@ -4,20 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.widget.TextView;
 import android.view.View;
-import android.widget.Button;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
-public class RealtiveLayout extends AppCompatActivity {
+public class RealtiveLayout extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realtive_layout);
-
-        // set TextView1 to Hello World
-        TextView text = findViewById(R.id.text1);
-        text.setText("HELLO WORLD");
     }
 
+    public void onTouch(View view)
+    {
+        view.setEnabled(false);
+    }
 }
