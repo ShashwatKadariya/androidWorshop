5b3f6040c3245b9c48c1dd754a59f132259442f3
ShashwatKadariya <kadariyasaswot@gmail.com>
2021-07-22 09:46:11 +0545

    first assignment

--- app/src/main/java/com/example/firstapp/FrameLayout.java
+++ app/src/main/java/com/example/firstapp/FrameLayout.java
@@ -1,14 +1,13 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FrameLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_table_layout);
+        setContentView(R.layout.activity_frame_layout);
     }
 }
--- app/src/main/java/com/example/firstapp/LinearActivity.java
+++ app/src/main/java/com/example/firstapp/LinearActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.firstapp.R.layout.*;
+
+public class LinearActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_table_layout);
+        setContentView(activity_linear);
     }
 }
